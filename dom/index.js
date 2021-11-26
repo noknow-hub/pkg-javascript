1f14d3e7ac61175b1dd8616c88a03f0fbc7e3801
Kakeru Sato <sato.kakeru3@gmail.com>
2021-11-26 12:15:30 +0400

    pre-release

--- dom/index.js
+++ dom/index.js
@@ -7,33 +7,33 @@
 //////////////////////////////////////////////////////////////////////
 class Dom {
 
+    static NAME_SPACE_URI_MATH_ML = 'http://www.w3.org/1998/Math/MathML';
+    static NAME_SPACE_URI_HTML = 'http://www.w3.org/1999/xhtml';
+    static NAME_SPACE_URI_SVG = 'http://www.w3.org/2000/svg';
+    static TAG_A = 'A';
+    static TAG_BUTTON = 'BUTTON';
+    static TAG_DIV = 'DIV';
+    static TAG_H1 = 'H1';
+    static TAG_H2 = 'H2';
+    static TAG_H3 = 'H3';
+    static TAG_H4 = 'H4';
+    static TAG_H5 = 'H5';
+    static TAG_H6 = 'H6';
+    static TAG_LI = 'LI';
+    static TAG_P = 'P';
+    static TAG_SPAN = 'SPAN';
+    static TAG_SVG = 'SVG';
+    static TAG_TABLE = 'TABLE';
+    static TAG_TD = 'TD';
+    static TAG_TH = 'TH';
+    static TAG_TR = 'TR';
+    static TAG_Ul = 'UL';
+    static TAG_USE = 'USE';
+
     //////////////////////////////////////////////////
     // Constructor
     //////////////////////////////////////////////////
     constructor() {
-        this.nameSpaceUriMathMl = 'http://www.w3.org/1998/Math/MathML';
-        this.nameSpaceUriHtml = 'http://www.w3.org/1999/xhtml';
-        this.nameSpaceUriSvg = 'http://www.w3.org/2000/svg';
-        this.tagA = 'A';
-        this.tagButton = 'BUTTON';
-        this.tagDiv = 'DIV';
-        this.tagH1 = 'H1';
-        this.tagH2 = 'H2';
-        this.tagH3 = 'H3';
-        this.tagH4 = 'H4';
-        this.tagH5 = 'H5';
-        this.tagH6 = 'H6';
-        this.tagLi = 'LI';
-        this.tagP = 'P';
-        this.tagSpan = 'SPAN';
-        this.tagSvg = 'SVG';
-        this.tagTable = 'TABLE';
-        this.tagTd = 'TD';
-        this.tagTh = 'TH';
-        this.tagTr = 'TR';
-        this.tagUl = 'UL';
-        this.tagUse = 'USE';
-
         //this.fragment = document.createDocumentFragment();
         this.htmlElement = null;
     }
@@ -94,26 +94,6 @@ class Dom {
 
 
     //////////////////////////////////////////////////////////////////////
-    // Set attribute list to the specified element.
-    // @params:
-    //     attrList: Array: e.g. [{ name: 'NAME1', value: 'VALUE1' }, { name: 'NAME2', value: 'VALUE2' }]
-    //     specifiedElm: Object: The specified HTML element.
-    //////////////////////////////////////////////////////////////////////
-    SetAttrListToSpecifiedElm(attrList, specifiedElm) {
-        if(attrList == null || attrList.length === 0) {
-            return this;
-        }
-        for(let i = 0; i < attrList.length; i++) {
-            const obj = attrList[i];
-            if(obj.hasOwnProperty('name') && obj.hasOwnProperty('value')) {
-                specifiedElm.setAttribute(obj.name, obj.value);
-            }
-        }
-        return this;
-    }
-
-
-    //////////////////////////////////////////////////////////////////////
     // Set class list.
     // @params:
     //     classList: Array: e.g. ["CLASS-A", "CLASS-B"].
@@ -121,20 +101,6 @@ class Dom {
     SetClassList(classList) {
         if(classList != null && classList.length > 0) {
             this.htmlElement.classList.add(...classList);
-        }
-        return this;
-    }
-
-
-    //////////////////////////////////////////////////////////////////////
-    // Set class list to the specified element.
-    // @params:
-    //     classList: Array: e.g. ["CLASS-A", "CLASS-B"].
-    //     specifiedElm: Object: The specified HTML element.
-    //////////////////////////////////////////////////////////////////////
-    SetClassListToSpecifiedElm(classList, specifiedElm) {
-        if(classList != null && classList.length > 0) {
-            specifiedElm.classList.add(...classList);
         }
         return this;
     }
@@ -162,24 +128,50 @@ class Dom {
 
 
     //////////////////////////////////////////////////////////////////////
-    // Set content list to the specified element.
+    // Set dataset.
     // @params:
-    //     contentList: Array: e.g. ["TEXT_A", "CLASS_B", HTML_ELEMENT_A].
-    //     specifiedElm: Object: The specified HTML element.
+    //     dataset: Object: e.g. { key1: "VAL1", key2: "VAL2" }.
     //////////////////////////////////////////////////////////////////////
-    SetContentListToSpecifiedElm(contentList, specifiedElm) { 
-        if(contentList == null || contentList.length === 0) {
+    SetDataset(dataset) {
+        if(dataset != null) {
             return this;
         }
-        for(let i = 0; i < contentList.length; i++) {
-            const v = contentList[i];
-            if(typeof v === 'string' || v instanceof String || typeof v === 'number' || typeof v === 'boolean' || typeof v === 'bigint') {
-                specifiedElm.textContent = v;
-            } else if(typeof v === 'object' && v.nodeName !== undefined) {
-                specifiedElm.appendChild(v);
-            }
+        for(const key in dataset) {
+            this.htmlElement.dataset[key] = dataset[key];
         }
         return this;
+    }
+
+
+    //////////////////////////////////////////////////////////////////////
+    // Set ID.
+    // @params:
+    //     id: String: "UNIQUE_ID"
+    //////////////////////////////////////////////////////////////////////
+    SetId(id) {
+        if(id != null) {
+            this.htmlElement.id = id;
+        }
+        return this;
+    }
+
+
+    //////////////////////////////////////////////////////////////////////
+    // Set attribute list to the specified element.
+    // @params:
+    //     attrList: Array: e.g. [{ name: 'NAME1', value: 'VALUE1' }, { name: 'NAME2', value: 'VALUE2' }]
+    //     specifiedElm: Object: The specified HTML element.
+    //////////////////////////////////////////////////////////////////////
+    static SetAttrListToSpecifiedElm(attrList, specifiedElm) {
+        if(attrList == null || attrList.length === 0) {
+            return;
+        }
+        for(let i = 0; i < attrList.length; i++) {
+            const obj = attrList[i];
+            if(obj.hasOwnProperty('name') && obj.hasOwnProperty('value')) {
+                specifiedElm.setAttribute(obj.name, obj.value);
+            }
+        }
     }
 
 
@@ -197,20 +189,37 @@ class Dom {
 
 
     //////////////////////////////////////////////////////////////////////
-    // Set dataset.
+    // Set class list to the specified element.
     // @params:
-    //     dataset: Object: e.g. { key1: "VAL1", key2: "VAL2" }.
+    //     classList: Array: e.g. ["CLASS-A", "CLASS-B"].
+    //     specifiedElm: Object: The specified HTML element.
     //////////////////////////////////////////////////////////////////////
-    static SetDataset(dataset) {
-        if(dataset != null) {
-            return this;
+    static SetClassListToSpecifiedElm(classList, specifiedElm) {
+        if(classList != null && classList.length > 0) {
+            specifiedElm.classList.add(...classList);
         }
-        for(const key in dataset) {
-            this.htmlElement.dataset[key] = dataset[key];
-        }
-        return this;
     }
 
+
+    //////////////////////////////////////////////////////////////////////
+    // Set content list to the specified element.
+    // @params:
+    //     contentList: Array: e.g. ["TEXT_A", "CLASS_B", HTML_ELEMENT_A].
+    //     specifiedElm: Object: The specified HTML element.
+    //////////////////////////////////////////////////////////////////////
+    static SetContentListToSpecifiedElm(contentList, specifiedElm) { 
+        if(contentList == null || contentList.length === 0) {
+            return;
+        }
+        for(let i = 0; i < contentList.length; i++) {
+            const v = contentList[i];
+            if(typeof v === 'string' || v instanceof String || typeof v === 'number' || typeof v === 'boolean' || typeof v === 'bigint') {
+                specifiedElm.textContent = v;
+            } else if(typeof v === 'object' && v.nodeName !== undefined) {
+                specifiedElm.appendChild(v);
+            }
+        }
+    }
 
 
     //////////////////////////////////////////////////////////////////////
@@ -229,19 +238,6 @@ class Dom {
         return this;
     }
 
-
-
-    //////////////////////////////////////////////////////////////////////
-    // Set ID.
-    // @params:
-    //     id: String: "UNIQUE_ID"
-    //////////////////////////////////////////////////////////////////////
-    static SetId(id) {
-        if(id != null) {
-            this.htmlElement.id = id;
-        }
-        return this;
-    }
 
 
     //////////////////////////////////////////////////////////////////////
