ad0cbc9772e17912a937613cc6c872c7d041c6e3
Kakeru Sato <sato.kakeru3@gmail.com>
2022-11-13 14:34:44 +0900

    dev

--- text-editor/index.js
+++ text-editor/index.js
@@ -2,19 +2,81 @@
 // index.js
 //////////////////////////////////////////////////////////////////////
 
-class Index {
+class TextEditor {
 
-    static NAME_SPACE_URI_MATH_ML = 'http://www.w3.org/1998/Math/MathML';
-    static NAME_SPACE_URI_HTML = 'http://www.w3.org/1999/xhtml';
-    static NAME_SPACE_URI_SVG = 'http://www.w3.org/2000/svg';
+    static CLASS_NAME = 'text-editor':
+    static OPTION_KEY_STATUS_BAR = 'status-bar';
+    static OPTION_KEY_STATUS_BAR_NUM_OF_CHARS = 'num-of-chars';
+    static OPTION_KEY_STATUS_BAR_NUM_OF_CHARS_TEXT = 'num-of-chars-text';
+    static OPTION_KEY_TOOL_BAR = 'tool-bar';
+    static OPTION_KEY_TOOL_BAR_BOLD = 'bold';
+    static OPTION_VAL_STATUS_BAR_NUM_OF_CHARS_TEXT = 'Chars';
 
     //////////////////////////////////////////////////////////////////////
     // Constructor
     //////////////////////////////////////////////////////////////////////
     constructor(targetElm, option = null) {
+        this.targetElm = targetElm;
+        if(this.targetElm === undefined || this.targetElm === null) {
+            console.log('[ERROR] Initialization error. The specified element could not be loaded.');
+            return;
+        }
+        this.option = {
+            [TextEditor.OPTION_KEY_STATUS_BAR]: {
+                [TextEditor.OPTION_KEY_STATUS_BAR_NUM_OF_CHARS]: false,
+                [TextEditor.OPTION_KEY_STATUS_BAR_NUM_OF_CHARS_TEXT]: TextEditor.OPTION_VAL_STATUS_BAR_NUM_OF_CHARS_TEXT,
+            },
+            [TextEditor.OPTION_KEY_TOOL_BAR]: {
+                [TextEditor.OPTION_KEY_TOOL_BAR_BOLD]: false
+            }
+        };
+
+        // Set option
+        this.setOption(option);
+
+        // Initialize
+        this.init();
+    }
+
+
+    //////////////////////////////////////////////////////////////////////
+    // Initialize
+    //////////////////////////////////////////////////////////////////////
+    init() {
+        this.targetElm.classList.add(TextEditor.CLASS_NAME);
+        
+        
+        
         
     }
 
 
+    //////////////////////////////////////////////////////////////////////
+    // Set option
+    //////////////////////////////////////////////////////////////////////
+    setOption(option) {
+        if(option === null) {
+            return;
+        }
+        for(let key in option) {
+            if(key === TextEditor.OPTION_KEY_STATUS_BAR) {
+                for(let key in option[TextEditor.OPTION_KEY_STATUS_BAR]) {
+                    if(key === TextEditor.OPTION_KEY_STATUS_BAR_NUM_OF_CHARS ||
+                            key === TextEditor.OPTION_KEY_STATUS_BAR_NUM_OF_CHARS_TEXT) {
+                        this.option[TextEditor.OPTION_KEY_STATUS_BAR][key] = option[TextEditor.OPTION_KEY_STATUS_BAR][key];
+                    }
+                }
+            } else if(key === TextEditor.OPTION_KEY_TOOL_BAR) {
+                for(let key in option[TextEditor.OPTION_KEY_TOOL_BAR]) {
+                    if(key === TextEditor.TextEditor.OPTION_KEY_TOOL_BAR_BOLD) {
+                        this.option[TextEditor.OPTION_KEY_TOOL_BAR][key] = option[TextEditor.OPTION_KEY_TOOL_BAR][key];
+                    }
+                }
+            }
+        }
+    }
+
+
+
 }
-export { Index };
+export { TextEditor };
